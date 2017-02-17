d4726202afec7bf156210ca848bd3666417a6a88
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-02-17 09:48:32 -0800

    create registrations controller. Products not saving to DB

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @products = Product.all
   end
@@ -12,13 +13,34 @@ class ProductsController < ApplicationController
  end
 
  def create
-    @product = Product.new(product_params)
+    @product = Product.new
     if @product.save
       flash[:notice] = "Product successfully added!"
-      redirect_to  product_path(@product)
+      redirect_to  product_path
     else
-      render :new
+      redirect_to root_path
     end
   end
+
+  def show
+    @user = current_user.id
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+   @user = current_user.id
+   @product = Product.find(params[:id])
+ end
+
+ def update
+    @user = current_user.id
+    @product = Product.find(params[:id])
+    if @product.update
+      redirect_to :back
+    else
+      render :edit
+    end
+  end
+
 
 end
